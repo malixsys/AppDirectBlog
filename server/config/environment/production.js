131fac6c118c7235b73abe2b2e070e37e11f280b
Martin Alix <github@malix.com>
2016-01-06 18:10:50 -0500

    fixing production

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,8 +3,8 @@
 var path = require('path');
 
 module.exports = {
-  ip: process.env.IP || undefined,
   mongo: {
     uri: 'tingodb://'+ path.join(__dirname, '..', '..','data')
+    //uri: 'mongodb://localhost/appdirectblog-dev'
   }
 };
